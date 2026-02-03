1cb65772570ba1549e0db653d70843ddc4810100
Ezra Mulaga <mulagaj@gmail.com>
2026-02-03 11:55:59 +1000

    Resolve syntax error in supabase Javascript that was using .env format

--- docs/src/js/config/supabase.js
+++ docs/src/js/config/supabase.js
@@ -1,9 +1,9 @@
 // src/js/config/supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-SUPABASE_URL=https://taicbbbjqgxrrtguirpx.supabase.co;
-SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhaWNiYmJqcWd4cnJ0Z3VpcnB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1NTc2NDAsImV4cCI6MjA4NDEzMzY0MH0.XBBVoX5D7vIbI7bvZnV9I1fbqjKpgsqSYWY-uIOo6SQ;
-
+const SUPABASE_URL = "https://taicbbbjqgxrrtguirpx.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhaWNiYmJqcWd4cnJ0Z3VpcnB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1NTc2NDAsImV4cCI6MjA4NDEzMzY0MH0.XBBVoX5D7vIbI7bvZnV9I1fbqjKpgsqSYWY-uIOo6SQ";
 
 export const supabase = createClient(
   SUPABASE_URL,
