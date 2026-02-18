2baa40208026241e8cf6aecd1f39aed65fe7a385
Ezra Mulaga <mulagaj@gmail.com>
2026-02-18 21:58:39 +1000

    Added access control logic in access-control.js

--- docs/src/js/auth/access-control.js
+++ docs/src/js/auth/access-control.js
@@ -4,10 +4,43 @@
  * Access rules and data handling are under active development.
  */
 
-const params = new URLSearchParams(window.location.search);
-const source = params.get("source");
+import { supabase } from "../config/supabase.js";
 
-if (source !== "emergency") {
-  window.location.replace("access-denied.html?reason=unauthorized_access");
+export async function requireAuth(redirectTo = ".././pages/user-login.html") {
+  const {data: {session}} = await supabase.auth.getSession();
+  if (!session){
+    window.location.href = redirectTo;
+    throw new Error("Not authenticated");
+  }
+  return session;
 }
 
+export async function getUserRole(authUserID) {
+  // Check if user is a practitioner
+  const {data: prac } = await supabase
+  .from("practitioners")
+  .select("practitioner_id, role, is_active")
+  .eq("auth_user_id", authUserID)
+  .maybesingle();
+  
+  if(prac?.is_active) return {type: "practitioner". ...prac};
+  // Check user is a patient
+  const {data: pu} = await supabase
+  .from("patient_users")
+  .select("patient_id, patient_user_id")
+  .eq("auth_user_id", authUserID)
+  .maybesingle();
+
+  if (pu) return {type: "patient", ...pu};
+
+  // return unkown if match not found.
+  return {type: "unkown"};
+
+}
+
+export async function logout() {
+  await supabase.auth.signout();
+  // Check location for error
+  window.location.href = "index.html"
+  
+}
--- docs/src/js/config/supabase.js
+++ docs/src/js/config/supabase.js
@@ -1,10 +1,12 @@
 // src/js/config/supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+// Database Credentials
 const SUPABASE_URL = "https://taicbbbjqgxrrtguirpx.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhaWNiYmJqcWd4cnJ0Z3VpcnB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1NTc2NDAsImV4cCI6MjA4NDEzMzY0MH0.XBBVoX5D7vIbI7bvZnV9I1fbqjKpgsqSYWY-uIOo6SQ";
 
+// Pushed out credentials.
 export const supabase = createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
